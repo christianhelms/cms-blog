1026314b1501a05cf717e1e1c103b09917390fa9
christianhelms <helmsmedia@gmail.com>
2022-02-11 21:54:59 -0600

    fixed category and post widget display. Was below posts now beside.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,12 @@ export default function Home({ posts }) {
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) => (<PostCard key={post.title} post={post.node}/>))}
         </div>
-      </div>
-      <div className='lg:col-span-4 col-span-1'>
+        <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
               <PostWidget />
               <Categories />
             </div>
+        </div>
       </div>
     </div>
   )
