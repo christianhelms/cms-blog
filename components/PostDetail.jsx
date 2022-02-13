25dc90c2af8c140061a20aed0ae2f22a8d74d6a3
christianhelms <helmsmedia@gmail.com>
2022-02-13 14:14:54 -0600

    setting up post details. Fixed runtime error

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const PostDetail = () => {
+const PostDetail = ({ post }) => {
   return (
-    <div>
-        <h1>
-            PostDetail
-        </h1>
+    <div className='bg-white shadow-lg rounded lg:p-8 pb-12 mb-8'>
+        <div className='relitive overflow-hidden shadow-md mb-6'>
+            <img 
+                src={post.featuredImage.url}
+                alt={post.title}
+                className='object-top height-full width-full rounded-t-lg'
+            />
+        </div>
     </div>
   )
 }
--- services/index.js
+++ services/index.js
@@ -38,6 +38,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug : String!) {
+      post(where: {slug: $slug}) {
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        author{
+          name
+          bio
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        content {
+          raw
+        }
+        categories {
+          name
+          slug
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql `
     query GetPostDetails() {
@@ -59,26 +93,26 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPosts = async () => {
-  const query = gql `
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
-        where: { slug_not: $slug, AND {categories_some: {slug_in: $categories}}}
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
         last: 3
-        ) {
-          title
-          featuredImage {
-            url
-          }
-          createdAT
-          slug
+      ) {
+        title
+        featuredImage {
+          url
         }
+        createdAt
+        slug
+      }
     }
-  `
-   const result = await request(graphqlAPI, query);
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
 
-   return result.posts;
-}
+  return result.posts;
+};
 
 export const getCategories = async () => {
   const query = gql `
