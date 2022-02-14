66d696adc5e6dcedd2d1e4828c52aae68908c3d9
christianhelms <helmsmedia@gmail.com>
2022-02-14 15:41:38 -0600

    added author box design and dynamic author picture

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-        <h1>
-            Author
-        </h1>
+    <div className='text-center mt-20 mb-8 p-12 relitive rounded-lg bg-black bg-opacity-20'>
+            <img 
+                alt={author.name}
+                height="100px"
+                width="100px"
+                className='align-middle rounded-full'
+                src={author.photo.url}
+            />
     </div>
   )
 }
