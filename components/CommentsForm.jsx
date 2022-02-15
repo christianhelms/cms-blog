4d85dce534b3029b6e24c065cd418390f06a3222
christianhelms <helmsmedia@gmail.com>
2022-02-15 15:48:22 -0600

    comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setlocalStorage] = useState(null);
+  const [showSuccessMessage, setSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDatatEl = useRef();
+  
+
   return (
     <div>
         <h1>
